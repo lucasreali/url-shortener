9129d9d96c48dc658816ff6e866d4fe33cde41c4
lucasreali <lucasrealioficial@gmail.com>
2026-07-03 14:04:44 -0300

    feat: add OriginalURL value object with validation

---
+++ src/main/java/com/example/urlshotener/domain/model/OriginalURL.java
@@ -0,0 +1,22 @@
+package com.example.urlshotener.domain.model;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class OriginalURL {
+    private final String url;
+
+    public OriginalURL(String url) {
+        if (url == null || url.isBlank()) {
+            throw new IllegalArgumentException("URL can't be blank");
+        }
+
+        try {
+            new URI(url).toURL();
+        }  catch (URISyntaxException | MalformedURLException exception) {
+            throw new IllegalArgumentException("Invalid URL: " + url);
+        }
+        this.url = url;
+    }
+}
