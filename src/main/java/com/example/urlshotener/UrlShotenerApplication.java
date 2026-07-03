3f47609621806e0b42b6212e801b40e453fe37c7
lucasreali <lucasrealioficial@gmail.com>
2026-07-03 13:16:16 -0300

    chore: initial project scaffold with spring boot and gradle

---
+++ src/main/java/com/example/urlshotener/UrlShotenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlshotener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShotenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShotenerApplication.class, args);
+    } 
+
+}
---
+++ src/test/java/com/example/urlshotener/UrlShotenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlshotener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShotenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
