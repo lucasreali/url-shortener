73fb4ef183f2ab0ddd425a70735d854df72236c8
lucasreali <lucasrealioficial@gmail.com>
2026-07-04 12:21:06 -0300

    feat: add ShortCodeGenerator output port

---
+++ src/main/java/com/example/urlshotener/application/port/out/ShortCodeGenerator.java
@@ -0,0 +1,7 @@
+package com.example.urlshotener.application.port.out;
+
+import com.example.urlshotener.domain.model.ShortCode;
+
+public interface ShortCodeGenerator {
+    ShortCode generate();
+}
