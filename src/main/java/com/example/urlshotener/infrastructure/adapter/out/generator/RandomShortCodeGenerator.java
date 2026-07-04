6ffa97ea73441ae92d53bbb8c21357bdb88754d0
lucasreali <lucasrealioficial@gmail.com>
2026-07-04 12:39:26 -0300

    feat: add RandomShortCodeGenerator adapter

---
+++ src/main/java/com/example/urlshotener/infrastructure/adapter/out/generator/RandomShortCodeGenerator.java
@@ -0,0 +1,24 @@
+package com.example.urlshotener.infrastructure.adapter.out.generator;
+
+import com.example.urlshotener.application.port.out.ShortCodeGenerator;
+import com.example.urlshotener.domain.model.ShortCode;
+
+import java.security.SecureRandom;
+
+public class RandomShortCodeGenerator implements ShortCodeGenerator {
+
+    @Override
+    public ShortCode generate() {
+        String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom random = new SecureRandom();
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(characters.length());
+            char character = characters.charAt(index);
+            builder.append(character);
+        }
+
+        return new ShortCode(builder.toString());
+    }
+}
