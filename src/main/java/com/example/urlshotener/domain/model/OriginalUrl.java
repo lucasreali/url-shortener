a746c772c1de0c7424a94c36a5c6abf5b07cccdc
lucasreali <lucasrealioficial@gmail.com>
2026-07-04 21:03:42 -0300

    refactor: convert OriginalUrl to record

--- src/main/java/com/example/urlshotener/domain/model/OriginalUrl.java
+++ src/main/java/com/example/urlshotener/domain/model/OriginalUrl.java
@@ -4,11 +4,8 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class OriginalUrl {
-
-    private final String url;
-
-    public OriginalUrl(String url) {
+public record OriginalUrl(String url) {
+    public OriginalUrl {
         if (url == null || url.isBlank()) {
             throw new IllegalArgumentException("URL can't be blank");
         }
@@ -18,6 +15,5 @@ public class OriginalUrl {
         }  catch (URISyntaxException | MalformedURLException exception) {
             throw new IllegalArgumentException("Invalid URL: " + url);
         }
-        this.url = url;
     }
 }
